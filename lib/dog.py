6c45e035f8fdc50a93df8d07525a321877d2588a
denniskioko <kiokodennis6990@gmail.com>
2024-03-05 08:29:47 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,19 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    def __init__(self, name=""):
+        self.name = ""
+        self.name = name
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+    
+fido = Dog("")
+#print(fido.name)
