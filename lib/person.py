f9613faa6f7878c923cecbde9ed21fb4831ed946
denniskioko <kiokodennis6990@gmail.com>
2024-03-05 08:38:54 +0300

    propertis

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,20 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, job=""):
+        self.job = ""
+        self.job = job
+
+    def job(self):
+        return self._job
+
+    def job(self, value):
+        if value in self.approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+            print("The job must be in the following list of jobs:")
+            print(self.approved_jobs)
+
+person1 = Person("ICT")
+print(person1.job)
